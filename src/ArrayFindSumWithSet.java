24988cea47e65c266816bd636fba7763c2bd5a67
Jan Gevaert <jan.gevaert@gmail.com>
2020-11-20 10:02:46 -0700

    Initial commit

---
+++ src/ArrayFindSumWithSet.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ArrayFindSumWithSet {
+// Given an array of ints find two elements that sum up to the given sum.
+
+  public static void main(String[] args) {
+    prettyPrint(getRandomArray(9), 11);
+    prettyPrint(getRandomArray(10), 12);
+  }
+
+  public static void printPairsUsingSet(int[] numbers, int n) {
+    if (numbers.length < 2) {
+      return;
+    }
+    Set set = new HashSet(numbers.length);
+
+    for (int number : numbers) {
+      int target = n - number;
+
+      if (!set.contains(target)) {
+        set.add(number);
+      } else {
+        System.out.printf("(%d, %d) %n", number, target);
+      }
+    }
+  }
+
+    public static void prettyPrint(int[] random, int k) {
+      System.out.println("Random Integer array : " + Arrays.toString(random));
+      System.out.println("Sum : " + k);
+      System.out.println("pair of numbers from an array whose sum equals " + k);
+      printPairsUsingSet(random, k);
+    }
+
+    public static int[] getRandomArray(int length) {
+    int[] randoms = new int[length];
+
+      for (int i = 0; i < length ; i++) {
+        randoms[i] = (int) (Math.random()*15);
+      }
+      return randoms;
+    }
+
+}
